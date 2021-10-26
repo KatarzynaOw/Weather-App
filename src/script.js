d97e76f1abf632af58a201052183c4ca5b0e6f44
KatarzynaOw <k.owczarek94@gmail.com>
2021-10-26 08:12:07 +0200

    Week 8 updates

--- src/script.js
+++ src/script.js
@@ -47,15 +47,6 @@ function displayWeather(response) {
   );
 }
 
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "6bfa54f242cbb59343d4e58db578dc61";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
-  console.log(apiUrl);
-  axios.get(apiUrl);
-}
-getForecast(response.data.coord);
-
 function searchCity(event) {
   event.preventDefault();
   let apiKey = "6bfa54f242cbb59343d4e58db578dc61";
@@ -91,7 +82,7 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -118,8 +109,16 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "6bfa54f242cbb59343d4e58db578dc61";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+getForecast(response.data.coord);
 
 let timeElement = document.querySelector("#current-time");
 let currentTime = new Date();
